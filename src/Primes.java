f192748378da69c3e9678a48d1645dba0e1dcde3
Nikita Shabanov <nshbnv@gmail.com>
2022-02-17 13:04:58 +0300

    Loop in main realized

--- src/Primes.java
+++ src/Primes.java
@@ -1,6 +1,9 @@
 public class Primes {
     public static void main(String[] args) {
-
+        for (int i = 2; i <= 100; i++)
+            if (isPrime(i)) {
+                System.out.println(i);
+            }
     }
 
     public static boolean isPrime(int n) {
