cf0660da19fab3909fb78e52b3b8ef9ce8bb0665
Nikita Shabanov <nshbnv@gmail.com>
2022-02-17 13:11:31 +0300

    Added explanations

--- src/Primes.java
+++ src/Primes.java
@@ -1,11 +1,14 @@
+//Main class
 public class Primes {
+    //Main method
     public static void main(String[] args) {
+        //looping value
         for (int i = 2; i <= 100; i++)
             if (isPrime(i)) {
                 System.out.println(i);
             }
     }
-
+    //checking number simple or not
     public static boolean isPrime(int n) {
         for (int i = 2; i < n; i++) {
             if (n % i == 0) {
